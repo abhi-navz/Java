8d2820a32cf532d79f16f65e97a4bd0d92b7d100
abhi-navz <abhinavg01441@gmail.com>
2024-01-03 16:38:25 +0530

    Primitive data types in java

--- 01beginner/App.java
+++ 01beginner/App.java
@@ -2,5 +2,24 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        // Variable and Data types.
+
+        // # Primitive Data types in java
+
+        String name = "Abhinav Gupta";
+        byte smallNum = 100; // 8 bit signed integer ranges from -2^8 to 2^8-1
+        short s = 10000; // 16 bit signed integer, value ranges from -2^16 to 2^16-1
+        int a = -59038590; // 32 bit signed integer value ranges from -2^32 to 2^32-1
+        long longNum = 897892348;// 64 bit signed integer value ranges from -2^64 to 2^64-1
+        short b = 6;
+        int sum = a + b;
+        char c = 'c';
+        float f = 3;
+        double decimalNum = 5.7;
+        boolean isLearning = true;
+        System.out.println("String:" + name + ",int type:" + sum + ",long type:" + longNum + ",char:" + c + ",double:"
+                + decimalNum + ",boolean:" + isLearning);
+
     }
 }
